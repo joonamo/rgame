ab6497dee2293cc96f4357a6adcca4f4954a8834
Joona Heinikoski <joona.heinikoski@reaktor.com>
2019-08-23 19:52:38 +0300

    Add huippis goal

---
+++ Assets/Huippis/HuippisGoal.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HuippisGoal : MonoBehaviour
+{
+    protected Collider myCollider;
+    protected GameManager gameManager;
+
+    protected int huippisEntered;
+    protected bool active;
+
+    public GameObject whatIsHuippis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCollider = GetComponent<Collider>();
+        //myCollider.enabled = false;
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter!");
+        if (other.tag == "Huippis")
+        {
+            Destroy(other.gameObject);
+            ++huippisEntered;
+        }
+        else if (other.tag == "Player") {
+            Deactivate();
+        }
+    }
+
+    public void Activate() {
+        myCollider.enabled = true;
+    }
+
+    public void Deactivate() {
+        myCollider.enabled = false;
+        for (int i = huippisEntered; i > 0; --i) {
+            Instantiate(whatIsHuippis, transform.position, transform.rotation);
+        }
+    }
+}
