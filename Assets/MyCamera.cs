8a7d35d0f82eca9694d54ef0e81c947c6381a76e
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2019-08-23 19:09:47 +0300

    Main camera to follow the player

---
+++ Assets/MyCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCamera : MonoBehaviour
+{
+    public GameObject player; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform CamTran = Camera.main.transform;
+        Transform PlayerTransform = player.transform;
+
+        CamTran.position = new Vector3(
+            PlayerTransform.position.x,
+            PlayerTransform.position.y + 15,
+            PlayerTransform.position.z - 12
+        );
+	}
+}
