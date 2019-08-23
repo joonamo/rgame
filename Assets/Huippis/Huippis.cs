45290dea1b1da8498e51a16eb71b17c180008808
Joona Heinikoski <joona.heinikoski@reaktor.com>
2019-08-23 18:55:47 +0300

    Very first iteration of POIs and flocking

---
+++ Assets/Huippis/Huippis.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Huippis : MonoBehaviour
+{
+    protected GameManager gameManager;
+    protected Vector3 currentDirection;
+    public float rotateSpeed = 10.0f;
+    public float speed = 3.0f;
+
+    protected CharacterController charController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+        gameManager = FindObjectOfType<GameManager>();
+        currentDirection = transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 flockDirection = Vector3.zero;
+        foreach (POI poi in gameManager.POIs) {
+            Vector3 diff = poi.transform.position - transform.position;
+            if (diff.magnitude < poi.range) {
+                flockDirection += diff.normalized * poi.attract;
+            }
+        }
+        currentDirection = Vector3.Slerp(currentDirection, flockDirection, rotateSpeed * Time.deltaTime);
+        currentDirection.y = 0;
+        currentDirection.Normalize();
+
+        charController.SimpleMove(currentDirection * speed);
+    }
+}
---
+++ Assets/Huippis/POI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class POI : MonoBehaviour
+{
+    public float range = 10.0f;
+    public float attract = 1.0f;
+
+    protected GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        gameManager.POIs.Add(this);
+    }
+
+    private void OnDestroy()
+    {
+        gameManager.POIs.Remove(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
