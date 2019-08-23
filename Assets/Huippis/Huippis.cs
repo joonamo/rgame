0f4fbd202f1a2e2b36087a5465a64f8cc3b0b1c6
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2019-08-23 22:13:33 +0300

    Fix default level

--- Assets/Huippis/Huippis.cs
+++ Assets/Huippis/Huippis.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Huippis : MonoBehaviour
 {
+    protected Collider myCollider;
     protected GameManager gameManager;
     protected Vector3 currentDirection;
     public float rotateSpeed = 10.0f;
@@ -14,6 +15,9 @@ public class Huippis : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        myCollider = GetComponent<Collider>();
+        myCollider.enabled = false;
+
         charController = GetComponent<CharacterController>();
         gameManager = FindObjectOfType<GameManager>();
         currentDirection = transform.forward;
@@ -23,9 +27,11 @@ public class Huippis : MonoBehaviour
     void Update()
     {
         Vector3 flockDirection = Vector3.zero;
-        foreach (POI poi in gameManager.POIs) {
+        foreach (POI poi in gameManager.POIs)
+        {
             Vector3 diff = poi.transform.position - transform.position;
-            if (diff.magnitude < poi.range) {
+            if (diff.magnitude < poi.range)
+            {
                 float decay = 1.0f - Mathf.Pow((diff.magnitude / poi.range), poi.exponent);
                 flockDirection += diff.normalized * poi.attract * decay;
                 flockDirection += poi.transform.forward * poi.directionMatch * decay;
@@ -36,5 +42,24 @@ public class Huippis : MonoBehaviour
         currentDirection.Normalize();
 
         charController.SimpleMove(currentDirection * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if (other.tag == "Droppable item")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+    public void Activate()
+    {
+        myCollider.enabled = true;
+    }
+
+    public void Deactivate()
+    {
+        myCollider.enabled = false;
     }
 }
