1055417982228c6f88c06599906223fbd7e3c929
Joona Heinikoski <joona.heinikoski@reaktor.com>
2019-08-25 14:56:44 +0300

    Add ghetto comic

---
+++ Assets/GameManager/GhettoComic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhettoComic : MonoBehaviour
+{
+    Vector3 initialPosition;
+    protected GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position;
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Time.deltaTime * -5.0f, 0,0);
+        if ((transform.position - initialPosition).x < -120) {
+            transform.position = initialPosition;
+        }
+
+        if (gameManager.GetGameState() != GameState.BEFORE_START) {
+            Destroy(gameObject);
+        }
+    }
+}
